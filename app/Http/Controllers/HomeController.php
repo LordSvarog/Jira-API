8cf1c656ad778162ebc092d595cfa5c7985d470b
Kurkov <ik@webberry.ru>
2019-09-26 16:38:28 +0300

    добавлен вывод на странице хом проектов

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -63,7 +63,7 @@ class HomeController extends Controller
 //			print("Error Occured! " . $e->getMessage());
 //		}
 
-
+		$arrProjects =[];
 		try {
 			$proj = new ProjectService(new ArrayConfiguration(
 				array(
@@ -74,12 +74,13 @@ class HomeController extends Controller
 
 				)
 			));
-			print_r($proj->get('AX'));
+
 			$pt = $proj->getAllProjects();
 
 
 			foreach ($pt as $p){
-
+				$projectInfo = $proj->get($p->key);
+				//print_r($projectInfo->lead["displayName"]);
 				try {
 					$us = new UserService();
 
@@ -91,10 +92,19 @@ class HomeController extends Controller
 						'maxResults' => 50, //max 1000
 						//'actionDescriptorId' => 1,
 					];
-
+					$arrProjects[$p->key]['project'] = [
+						'pName'=> $projectInfo->name,
+						'lead'=> $projectInfo->lead["displayName"],
+						'avatarUrls'=> $projectInfo->avatarUrls["48x48"]
+					];
 					$users = $us->findAssignableUsers($paramArray);
 					foreach ($users as $user){
-						print_r("</br>".$p->name."-".$user->displayName."</br>");
+						$arrProjects[$p->key]['users'][$user->key] = [
+							'userName'=>$user->displayName,
+							'avatarUrls'=> (array)$user->avatarUrls
+						] ;
+
+
 					}
 
 				} catch (JiraException $e) {
@@ -106,6 +116,12 @@ class HomeController extends Controller
 			print("Error Occured! " . $e->getMessage());
 		}
 
+//		var_dump($arrProjects);
+
+		$arrProjects['count']=1;
+        return view('home',[
+			'arrProjects' => $arrProjects,
+		]);
 
 
 
@@ -115,144 +131,6 @@ class HomeController extends Controller
 
 
 
-
-
-
-//		try {
-//			$us = new UserService();
-//
-//			$paramArray = [
-//				//'username' => null,
-//				'project' => 'AX',
-//				//'issueKey' => 'TEST-1',
-//				'startAt' => 0,
-//				'maxResults' => 50, //max 1000
-//				//'actionDescriptorId' => 1,
-//			];
-//
-//			$users = $us->findAssignableUsers($paramArray);
-//			foreach ($users as $user){
-//				print_r($user->displayName);
-//			}
-//
-//		} catch (JiraException $e) {
-//			print("Error Occured! " . $e->getMessage());
-//		}
-
-
-
-
-
-
-
-
-//		try {
-//			$us = new UserService();
-//
-//			$paramArray = [
-//				'username' => '', // get all users.
-//				'startAt' => 0,
-//				'maxResults' => 1000,
-//				'includeInactive' => true,
-////
-//			];
-//
-//			// get the user info.
-//			$users = $us->findUsers($paramArray);
-//			print_r($users);
-//		} catch (JiraException $e) {
-//			print("Error Occured! " . $e->getMessage());
-//		}
-//
-//
-//
-//		foreach ($users as $user){
-//
-//			try {
-//				$us = new UserService();
-//
-//				$user = $us->get(['username' => $user->name]);
-//
-//				var_dump($user);
-//			} catch (JiraException $e) {
-//				print("Error Occured! " . $e->getMessage());
-//			}
-//
-//		}
-
-
-//		foreach ($pt as $p) {
-//			try {
-//				$queryParam = [
-//					'groupname' => 'WEB',
-//					'includeInactiveUsers' => true, // default false
-//					'startAt' => 0,
-//					'maxResults' => 50,
-//				];
-//
-//
-//				$proj = new GroupService(new ArrayConfiguration(
-//					array(
-//						'jiraHost' => 'https://webberry.atlassian.net',
-//						// for basic authorization:
-//						'jiraUser' => 'dl@webberry.ru',
-//						'jiraPassword' => 'LT3Rn5voDqd6C10SPvmXA7B9',
-//
-//					)
-//				));
-//				$ret = $proj->getMembers($queryParam);
-//
-//				print_r($ret);
-//			} catch (JiraException $e) {
-//				print("Error Occured! " . $e->getMessage());
-//			}
-//		}
-//		$iss = new IssueService(new ArrayConfiguration(
-//			array(
-//				'jiraHost' => 'https://webberry.atlassian.net',
-//				// for basic authorization:
-//				'jiraUser' => 'dl@webberry.ru',
-//				'jiraPassword' => 'LT3Rn5voDqd6C10SPvmXA7B9',
-//				// to enable session cookie authorization (with basic authorization only)
-//				'cookieAuthEnabled' => true,
-//				'cookieFile' => storage_path('jira-cookie.txt'),
-//				// if you are behind a proxy, add proxy settings
-//				"proxyServer" => 'your-proxy-server',
-//				"proxyPort" => 'proxy-port',
-//				"proxyUser" => 'proxy-username',
-//				"proxyPassword" => 'proxy-password',
-//			)
-//		));
-//
-//		$log = new Auth\AuthService();
-//		//$log ->login('ik@webberry.ru','LT3Rn5voDqd6C10SPvmXA7B9');
-//
-//		 $arr = array();
-//		array_push($arr,'dl@webberry.ru');
-//
-//
-//
-//		try {
-//			$proj = new ProjectService();
-//
-//			$prjs = $proj->getAllProjects();
-//
-//			foreach ($prjs as $p) {
-//				echo sprintf("Project Key:%s, Id:%s, Name:%s, projectCategory: %s\n",
-//					$p->key, $p->id, $p->name, $p->projectCategory['name']
-//				);
-//			}
-//		} catch (JiraException $e) {
-//			print("Error Occured! " . $e->getMessage());
-//		}
-//
-//
-//
-//		echo "<pre>";
-//			var_dump($log);
-//		echo "</pre>";
-//
-        return view('home');
     }
 
 }
